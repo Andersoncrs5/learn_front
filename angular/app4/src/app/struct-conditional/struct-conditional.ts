7bb8b91ad249281e2d85c748f1d5e5e7f051b5e7
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-27 15:40:08 -0300

    mgIf

--- angular/app4/src/app/struct-conditional/struct-conditional.ts
+++ angular/app4/src/app/struct-conditional/struct-conditional.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './struct-conditional.scss'
 })
 export class StructConditional {
+  showDiv: boolean = true
 
+  showDivToggle() {
+    this.showDiv = !this.showDiv
+  }
 }
